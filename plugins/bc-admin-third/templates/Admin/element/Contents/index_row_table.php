10f0725ba3241ee16840832f4c08a9b60fc7bc23
humuhimi <masa19951009@gmail.com>
2021-08-05 17:18:44 +0800

    'escape' => false を追加

--- plugins/bc-admin-third/templates/Admin/element/Contents/index_row_table.php
+++ plugins/bc-admin-third/templates/Admin/element/Contents/index_row_table.php
@@ -60,7 +60,7 @@ if ($data->self_status) {
 <tr id="Row<?= $count + 1 ?>"<?php $this->BcListTable->rowClass($isPublish, $data) ?>>
   <td class="bca-table-listup__tbody-td bca-table-listup__tbody-td--select"><?php // 選択 ?>
     <?php if ($this->BcBaser->isAdminUser() && empty($data->site_root)): ?>
-      <?= $this->BcAdminForm->control('ListTool.batch_targets.' . $data->id, ['type' => 'checkbox', 'label' => '<span class="bca-visually-hidden">チェックする</span>', 'class' => 'batch-targets bca-checkbox__input', 'value' => $data->id]) ?>
+      <?= $this->BcAdminForm->control('ListTool.batch_targets.' . $data->id, ['type' => 'checkbox', 'label' => '<span class="bca-visually-hidden">チェックする</span>', 'class' => 'batch-targets bca-checkbox__input', 'value' => $data->id, 'escape' => false]) ?>
     <?php endif ?>
   </td>
   <td class="bca-table-listup__tbody-td" style="width:5%"><?= $data->id; ?></td>
