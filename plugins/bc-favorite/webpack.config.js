5e8d4cd76f2b1902421b540606957efd4bc1166d
humuhimi <masa19951009@gmail.com>
2021-10-05 13:51:49 +0800

    bundleファイルの調整&&index.vueの編集

--- plugins/bc-favorite/webpack.config.js
+++ plugins/bc-favorite/webpack.config.js
@@ -1,12 +1,22 @@
 
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const glob = require("glob");
+var entries = {};
 const path = require('path');
 
+glob.sync("./webroot/js/src/**/*.js").map(function(file){
+    if(!file.replace('./webroot/js/src/admin/', '').match(/^_/)) {
+        entries[file.replace('./webroot/js/src/', '').split('.').shift()] = file;
+    }
+});
+
 module.exports = (env, argv) => {
+    console.log("aaa" + __dirname);
     return {
-        entry: ['@babel/polyfill', './webroot/js/src/admin/favorites/main.js'],
+        // entry: ['@babel/polyfill', './webroot/js/src/admin/favorites/main.js'],
+        entry: entries,
         output: {
-            filename: 'bundle.js',
+            filename: '[name].bundle.js',
             path: path.join(__dirname, 'webroot/js')
         },
         plugins: [
--- plugins/bc-favorite/webroot/js/src/admin/favorites/index.vue
+++ plugins/bc-favorite/webroot/js/src/admin/favorites/index.vue
@@ -1,37 +1,41 @@
 <template>
-    <h2 class="bca-nav-favorite-title">
-        <button type="button" id="btn-favorite-expand" class="bca-collapse__btn bca-nav-favorite-title-button"
-                data-bca-collapse="favorite-collapse" data-bca-target="#favoriteBody" aria-expanded="false"
-                aria-controls="favoriteBody" data-bca-state="<?php echo ($favoriteBoxOpened)? "open" : '' ?>">
-        <?php echo __d('baser', 'お気に入り') ?> <i class="bca-icon--chevron-down bca-nav-favorite-title-icon"></i>
-        </button>
-    </h2>
+    <div>
+        <h2 class="bca-nav-favorite-title">
+            <button type="button" id="btn-favorite-expand" class="bca-collapse__btn bca-nav-favorite-title-button"
+                    data-bca-collapse="favorite-collapse" data-bca-target="#favoriteBody" aria-expanded="false"
+                    aria-controls="favoriteBody" :data-bca-state="favoriteBoxOpened">
+            {{ favoriteTitle }} <i class="bca-icon--chevron-down bca-nav-favorite-title-icon"></i>
+            </button>
+        </h2>
 
-    <ul class="favorite-menu-list bca-nav-favorite-list bca-collapse" id="favoriteBody">
-        <?php if (!empty($favorites)): ?>
-        <?php foreach($favorites as $favorite): ?>
-            <?php $this->BcBaser->element('favorite_menu_row', ['favorite' => $favorite]) ?>
-        <?php endforeach ?>
+        <ul v-if="favorites" class="favorite-menu-list bca-nav-favorite-list bca-collapse" id="favoriteBody">
+            <li v-for="(favorite, i) in favorites" :key="i" :id="'FavoriteRow' + name" class="bca-nav-favorite-list-item">
+                <a :href="url" :title="fullUrl"><span class="bca-nav-favorite-list-item-label">{{name}}</span></a>
+                <hidden :value="id" class="favorite-id"  :name="'id' + '.' + id" />
+                <hidden :value="name" class="favorite-name"  :name="'name' + '.' + id" />
+                <hidden :value="url" class="favorite-url"  :name="'url' + '.' + id" />
+            </li>
+        </ul>
+        <ul v-else class="favorite-menu-list bca-nav-favorite-list bca-collapse" id="favoriteBody">
+            <li  class="no-data"><small>{{ noDataMessage }}</small></li>
+        </ul>
 
-        <?php else: ?>
-        <li class="no-data"><small><?php echo __d('baser', '「お気に入りに追加」ボタンよりお気に入りを登録しておく事ができます。') ?></small></li>
-        <?php endif ?>
-    </ul>
-    <div id="FavoriteDialog" title="お気に入り登録" style="display:none">
-    <?php echo $this->BcAdminForm->create('Favorite', ['url' => ['plugin' => null, 'action' => 'ajax']]) ?>
-    <?php echo $this->BcAdminForm->control('Favorite.id', ['type' => 'hidden']) ?>
-    <dl>
-        <dt><?php echo $this->BcForm->label('Favorite.name', __d('baser', 'タイトル')) ?></dt>
-        <dd><?php echo $this->BcAdminForm->control('Favorite.name', ['type' => 'text', 'size' => 30, 'class' => 'required']) ?></dd>
-        <dt><?php echo $this->BcForm->label('Favorite.url', __d('baser', 'URL')) ?></dt>
-        <dd><?php echo $this->BcAdminForm->control('Favorite.url', ['type' => 'text', 'size' => 30, 'class' => 'required']) ?></dd>
-    </dl>
-    <?php echo $this->BcAdminForm->end() ?>
+        <div id="FavoriteDialog" title="お気に入り登録" style="display:none">
+        <?php echo $this->BcAdminForm->create('Favorite', ['url' => ['plugin' => null, 'action' => 'ajax']]) ?>
+        <?php echo $this->BcAdminForm->control('Favorite.id', ['type' => 'hidden']) ?>
+        <dl>
+            <dt><?php echo $this->BcForm->label('Favorite.name', __d('baser', 'タイトル')) ?></dt>
+            <dd><?php echo $this->BcAdminForm->control('Favorite.name', ['type' => 'text', 'size' => 30, 'class' => 'required']) ?></dd>
+            <dt><?php echo $this->BcForm->label('Favorite.url', __d('baser', 'URL')) ?></dt>
+            <dd><?php echo $this->BcAdminForm->control('Favorite.url', ['type' => 'text', 'size' => 30, 'class' => 'required']) ?></dd>
+        </dl>
+        <?php echo $this->BcAdminForm->end() ?>
+        </div>
+
+
+        <ul id="FavoritesMenu" class="context-menu" style="display:none">
+            <li class="edit"><?php $this->BcBaser->link(__d('baser', '編集'), '#FavoriteEdit') ?></li>
+            <li class="delete"><?php $this->BcBaser->link(__d('baser', '削除'), '#FavoriteDelete') ?></li>
+        </ul>
     </div>
-
-
-    <ul id="FavoritesMenu" class="context-menu" style="display:none">
-    <li class="edit"><?php $this->BcBaser->link(__d('baser', '編集'), '#FavoriteEdit') ?></li>
-    <li class="delete"><?php $this->BcBaser->link(__d('baser', '削除'), '#FavoriteDelete') ?></li>
-    </ul>
 </template>
