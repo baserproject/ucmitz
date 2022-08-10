14314ba5dfd79d28fffe01d3b8496ef5f6c68a35
Đỗ Văn Hùng <HungDV2022>
2022-08-10 12:46:21 +0900

    fix #723 コールバック処理の実行を確認

--- plugins/baser-core/tests/TestCase/TestSuite/BcTestCaseTest.php
+++ plugins/baser-core/tests/TestCase/TestSuite/BcTestCaseTest.php
@@ -12,6 +12,8 @@
 namespace BaserCore\Test\TestCase\TestSuite;
 
 use BaserCore\Utility\BcContainer;
+use BaserCore\View\Helper\BcFormHelper;
+use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Cake\Http\Session;
 use Cake\Core\Configure;
@@ -19,6 +21,7 @@ use Cake\Log\Log;
 use Cake\Routing\Router;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Controller\AnalyseController;
+use Cake\View\View;
 
 /**
  * BaserCore\TestSuite\BcTestCase
@@ -184,7 +187,13 @@ class BcTestCaseTest extends BcTestCase
      * @return void
      */
     public function testEntryEventToMock(){
-        $rs = self::entryEventToMock(self::EVENT_LAYER_CONTROLLER, "add", "test");
+
+        $form = new BcFormHelper(new View());
+        $rs = self::entryEventToMock(self::EVENT_LAYER_HELPER, 'Form.afterEnd', function(Event $event){
+            $event->setData('out', 'test');
+        });
+
+        $this->assertEquals('test', $form->end());
         $this->assertTrue(isset($rs->layer));
         $this->assertTrue(isset($rs->plugin));
     }
