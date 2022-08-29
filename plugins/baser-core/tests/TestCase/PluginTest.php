a081f87c71b97b3d47b0c3dd622c82f0af056a5d
Đỗ Văn Hùng <HungDV2022>
2022-08-29 19:03:10 +0900

    fix #818 コメントの修正

--- plugins/baser-core/tests/TestCase/PluginTest.php
+++ plugins/baser-core/tests/TestCase/PluginTest.php
@@ -23,8 +23,7 @@ use Cake\Http\MiddlewareQueue;
 use Authentication\Middleware\AuthenticationMiddleware;
 use Cake\Http\ServerRequest;
 use Cake\Routing\Router;
-use Composer\Config;
-use http\Env;
+use Cake\Filesystem\File;
 
 /**
  * Class PluginTest
@@ -84,7 +83,6 @@ class PluginTest extends BcTestCase
     public function testBootStrap(): void
     {
         $event = EventManager::instance();
-        $this->Plugin->bootstrap($this->application);
 
         $this->assertTrue(is_array($event->listeners('Controller.initialize')));
 
@@ -104,7 +102,6 @@ class PluginTest extends BcTestCase
         $pathsPluginsExpected = [
             '/var/www/html/plugins/',
             '/var/www/html/vendor/baserproject/',
-            '/var/www/html/vendor/baserproject/',
         ];
 
         $this->assertEquals($pathsPluginsExpected, Configure::read('App.paths.plugins'));
@@ -120,13 +117,86 @@ class PluginTest extends BcTestCase
         $this->assertNotNull(\Cake\Core\Plugin::getCollection()->get('DebugKit'));
         $this->assertEquals('/var/www/html/plugins/' . Configure::read('BcApp.defaultFrontTheme') . '/templates/', Configure::read('App.paths.templates')[0]);
 
+        $file = new File('config/'. '.env');
+        $file->write('#!/usr/bin/env bash
+# Used as a default to seed config/.env which
+# enables you to use environment variables to configure
+# the aspects of your application that vary by
+# environment.
+#
+# Having this file in production is considered a **SECURITY RISK** and also decreases
+# the boostrap performance of your application.
+#
+# To use this file, first copy it into `config/.env`. Also ensure the related
+# code block for loading this file is uncommented in `config/boostrap.php`
+#
+# In development .env files are parsed by PHP
+# and set into the environment. This provides a simpler
+# development workflow over standard environment variables.
+export APP_NAME="ucmitz"
+export DEBUG="true"
+export APP_ENCODING="UTF-8"
+export APP_DEFAULT_LOCALE="en_US"
+export APP_DEFAULT_TIMEZONE="Asia/Tokyo"
+
+# baserCMS Settings
+export INSTALL_MODE="false"
+export SITE_URL="https://localhost/"
+export SSL_URL="https://localhost/"
+export ADMIN_SSL="true"
+export UPDATE_KEY="update"
+export ADMIN_PREFIX="admin"
+export BASER_CORE_PREFIX="baser"
+export SQL_LOG="false"
+
+# Uncomment these to define cache configuration via environment variables.
+#export CACHE_DURATION="+2 minutes"
+#export CACHE_DEFAULT_URL="file://tmp/cache/?prefix=${APP_NAME}_default&duration=${CACHE_DURATION}"
+#export CACHE_CAKECORE_URL="file://tmp/cache/persistent?prefix=${APP_NAME}_cake_core&serialize=true&duration=${CACHE_DURATION}"
+#export CACHE_CAKEMODEL_URL="file://tmp/cache/models?prefix=${APP_NAME}_cake_model&serialize=true&duration=${CACHE_DURATION}"
+
+# Uncomment these to define email transport configuration via environment variables.
+#export EMAIL_TRANSPORT_DEFAULT_URL=""
+
+# Uncomment these to define database configuration via environment variables.
+#export DATABASE_URL="mysql://my_app:secret@localhost/${APP_NAME}?encoding=utf8&timezone=UTC&cacheMetadata=true&quoteIdentifiers=false&persistent=false"
+#export DATABASE_TEST_URL="mysql://my_app:secret@localhost/test_${APP_NAME}?encoding=utf8&timezone=UTC&cacheMetadata=true&quoteIdentifiers=false&persistent=false"
+
+# Uncomment these to define logging configuration via environment variables.
+#export LOG_DEBUG_URL="file://logs/?levels[]=notice&levels[]=info&levels[]=debug&file=debug"
+#export LOG_ERROR_URL="file://logs/?levels[]=warning&levels[]=error&levels[]=critical&levels[]=alert&levels[]=emergency&file=error"
+
+        ');
+        $file->close();
+
+        $fileSetting = new File('config/'. 'setting.php');
+        $fileSetting->write('<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+/**
+ * setting
+ *
+ * plugins/baser-core/config/setting.php の値をカスタマイズする場合は、ここに定義する。
+ */
+
+return [];
+');
+
         $this->loginAdmin($this->getRequest('/baser/admin'));
         $this->Plugin->bootstrap($this->application);
         $this->assertEquals('/var/www/html/plugins/' . Configure::read('BcApp.defaultAdminTheme') . '/templates/', Configure::read('App.paths.templates')[0]);
 
-        if (file_exists(CONFIG . 'setting.php')) {
-            $this->assertTrue(Configure::isConfigured('baser'));
-        }
+        $this->assertNotNull(\Cake\Core\Plugin::getCollection()->get('DebugKit'));
+
+        $this->assertTrue(Configure::isConfigured('baser'));
     }
 
     /**
