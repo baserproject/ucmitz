93aa42c1d19588212af4874a9c478647995d1027
C. LE <chuong.le.jp@gmail.com>
2022-09-02 18:17:28 +0900

    fix #857 remove unused commas

--- plugins/baser-core/tests/TestCase/Service/ContentsServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/ContentsServiceTest.php
@@ -925,8 +925,8 @@ class ContentsServiceTest extends BcTestCase
      */
     public function testGetTitlesById()
     {
-        ContentFactory::make(['id' => 110, 'title' => 'ID110',], 1)->persist();
-        ContentFactory::make(['id' => 111, 'title' => 'ID111',], 1)->persist();
+        ContentFactory::make(['id' => 110, 'title' => 'ID110'], 1)->persist();
+        ContentFactory::make(['id' => 111, 'title' => 'ID111'], 1)->persist();
         $titles = $this->ContentsService->getTitlesById([110, 111]);
         $this->assertCount(2, $titles);
         $this->assertEquals('ID110', $titles[110]);
