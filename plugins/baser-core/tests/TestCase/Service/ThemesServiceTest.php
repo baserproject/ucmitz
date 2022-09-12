bf9e22f5eb0bebb4aa4ec8ba8aa336d0528eac58
Chuong Le <chuongle@Chuongs-MacBook-Pro.local>
2022-09-12 18:59:22 +0900

    fix #891 ThemesService::createDownloadToTmp() のユニットテスト（dev-#8）

--- plugins/baser-core/tests/TestCase/Service/ThemesServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/ThemesServiceTest.php
@@ -158,6 +158,23 @@ class ThemesServiceTest extends \BaserCore\TestSuite\BcTestCase
     }
 
     /**
+     * 指定したテーマをダウンロード用のテーマとして一時フォルダに作成する
+     * @return void
+     */
+    public function testCreateDownloadToTmp()
+    {
+        $tmpDir = TMP . 'theme' . DS;
+        $theme = 'BcFront';
+        $tmpThemeDir = $tmpDir . $theme;
+        $folder = new Folder();
+        $folder->delete($tmpThemeDir);
+
+        $result = $this->ThemesService->createDownloadToTmp($theme);
+        $this->assertEquals($tmpDir, $result);
+        $this->assertTrue(is_dir($tmpThemeDir));
+    }
+
+    /**
      * 一覧データ取得
      */
     public function testGetIndex()
