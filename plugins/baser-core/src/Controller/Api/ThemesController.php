7703f4167be82c85fcf2db93ab18a0b830384cff
Đỗ Văn Hùng <HungDV2022>
2022-09-12 18:58:34 +0900

    fix #932  テーマを適用するAPIを実装（dev-#8） APIを実装

---
+++ plugins/baser-core/src/Controller/Api/ThemesController.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Controller\Api;
+
+use BaserCore\Error\BcException;
+use BaserCore\Service\SitesServiceInterface;
+use BaserCore\Service\ThemesServiceInterface;
+use BaserCore\Annotation\UnitTest;
+use BaserCore\Annotation\NoTodo;
+use BaserCore\Annotation\Checked;
+
+/**
+ * Class ThemesController
+ *
+ * https://localhost/baser/api/baser-core/themes/action_name.json で呼び出す
+ *
+ * @package BaserCore\Controller\Api
+ */
+class ThemesController extends BcApiController
+{
+
+    /**
+     * [API] テーマを適用するAPI
+     * @param ThemesServiceInterface $themesService
+     * @param SitesServiceInterface $sitesService
+     * @param int $siteId
+     * @param string $theme
+     *
+     * @noTodo
+     */
+    public function apply(ThemesServiceInterface $themesService, SitesServiceInterface $sitesService, int $siteId, string $theme)
+    {
+        $this->request->allowMethod(['post']);
+
+        $errors = null;
+
+        if (!$theme || !is_numeric($siteId)) {
+            $this->setResponse($this->response->withStatus(400));
+            $message = __d('baser', 'テーマの適用に失敗しました。');
+        } else {
+            try {
+                $info = $themesService->apply($sitesService->get($siteId), $theme);
+                $message = [__d('baser', 'テーマ「{0}」を適用しました。', $theme)];
+                if ($info) $message = array_merge($message, [''], $info);
+                $message = implode("\n", $message);
+            } catch (BcException $e) {
+                $errors = $e->getMessage();
+                $message = __d('baser', 'テーマの適用に失敗しました。', $e->getMessage());
+            }
+        }
+
+        $this->set([
+            'message' => $message,
+            'theme' => $theme,
+            'errors' => $errors
+        ]);
+
+        $this->viewBuilder()->setOption('serialize', ['message', 'theme', 'errors']);
+    }
+
+}
