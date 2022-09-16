3bd3dd084e2274adb5c4af0ffe6fe44495d3a5e4
Chuong Le <chuongle@Chuongs-MacBook-Pro.local>
2022-09-16 16:10:55 +0900

    fix #916 BcDatabaseService::resetAllTables() のユニットテスト（dev-#8）

--- plugins/baser-core/src/Service/BcDatabaseService.php
+++ plugins/baser-core/src/Service/BcDatabaseService.php
@@ -204,6 +204,7 @@ class BcDatabaseService implements BcDatabaseServiceInterface
      * @return boolean
      * @noTodo
      * @checked
+     * @unitTest
      */
     public function resetAllTables($excludes = [])
     {
