4411e175084bd098473831a64fcacb9bfd8d930a
Đỗ Văn Hùng <HungDV2022>
2022-09-21 17:44:53 +0900

    fix #1005 ユーティリティ：ツリー構造リセットAPIを実装 #1005

--- plugins/baser-core/src/Controller/Api/UtilitiesController.php
+++ plugins/baser-core/src/Controller/Api/UtilitiesController.php
@@ -14,10 +14,40 @@ namespace BaserCore\Controller\Api;
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
 use BaserCore\Annotation\Checked;
+use BaserCore\Service\UtilitiesServiceInterface;
 
 /**
- * UtilitiesController
+ * Class UtilitiesController
+ *
+ * https://localhost/baser/api/baser-core/utilities/action_name.json で呼び出す
+ *
+ * @package BaserCore\Controller\Api
  */
 class UtilitiesController extends BcApiController
 {
+
+    /**
+     * [API] ユーティリティ：ツリー構造リセット
+     * @param UtilitiesServiceInterface $service
+     * @checked
+     * @noTodo
+     * @unitTest
+     */
+    public function reset_contents_tree(UtilitiesServiceInterface $service)
+    {
+        $this->request->allowMethod(['post']);
+
+        if ($service->resetContentsTree()) {
+            $message = __d('baser', 'コンテンツのツリー構造をリセットしました。');
+        } else {
+            $this->setResponse($this->response->withStatus(400));
+            $message = __d('baser', 'コンテンツのツリー構造のリセットに失敗しました。');
+        }
+
+        $this->set([
+            'message' => $message
+        ]);
+        $this->viewBuilder()->setOption('serialize', ['message']);
+    }
+
 }
