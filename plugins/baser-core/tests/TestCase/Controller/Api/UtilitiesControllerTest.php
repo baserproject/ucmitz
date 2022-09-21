7989ca0b4109ea31b1ff0ded0da69a970e19a167
Đỗ Văn Hùng <HungDV2022>
2022-09-21 18:49:32 +0900

    fix #1005 ユーティリティ：ツリー構造リセットAPIを実装

---
+++ plugins/baser-core/tests/TestCase/Controller/Api/UtilitiesControllerTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Controller\Api;
+
+use BaserCore\Service\UtilitiesService;
+use BaserCore\Test\Factory\ContentFactory;
+use BaserCore\Test\Scenario\InitAppScenario;
+use BaserCore\TestSuite\BcTestCase;
+use Cake\Core\Configure;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
+use Cake\TestSuite\IntegrationTestTrait;
+
+class UtilitiesControllerTest extends BcTestCase
+{
+
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/Contents',
+    ];
+
+    /**
+     * Access Token
+     * @var string
+     */
+    public $accessToken = null;
+
+    /**
+     * Refresh Token
+     * @var null
+     */
+    public $refreshToken = null;
+
+    /**
+     * set up
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $token = $this->apiLoginAdmin(1);
+        $this->accessToken = $token['access_token'];
+        $this->refreshToken = $token['refresh_token'];
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        Configure::clear();
+        parent::tearDown();
+    }
+
+    /**
+     * test reset_contents_tree
+     * @return void
+     */
+    public function test_reset_contents_tree()
+    {
+        ContentFactory::make(['id' => 1, 'name' => 'BaserCore root', 'type' => 'ContentFolder', 'site_root' => 1, 'lft' => 1, 'rght' => 2])->persist();
+        ContentFactory::make(['name' => 'BaserCore 1', 'type' => 'ContentFolder', 'site_root' => 1, 'lft' => 11, 'rght' => 12])->persist();
+        ContentFactory::make(['name' => 'BaserCore 2', 'type' => 'ContentFolder', 'site_root' => 1, 'lft' => 13, 'rght' => 14])->persist();
+
+        $this->post('/baser/api/baser-core/utilities/reset_contents_tree.json?token=' . $this->accessToken);
+        $this->assertResponseOk();
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertEquals('コンテンツのツリー構造をリセットしました。', $result->message);
+    }
+}
