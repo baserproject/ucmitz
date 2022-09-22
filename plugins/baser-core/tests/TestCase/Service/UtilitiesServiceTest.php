1d3f4c1669f45315f0992e3ad88c65c32ce1ea9b
Đỗ Văn Hùng <HungDV2022>
2022-09-22 12:58:06 +0900

    fix #1005 ユーティリティ：ツリー構造リセットAPIを実装

--- plugins/baser-core/tests/TestCase/Service/UtilitiesServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/UtilitiesServiceTest.php
@@ -40,7 +40,15 @@ class UtilitiesServiceTest extends BcTestCase
      * @var array
      */
     public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
         'plugin.BaserCore.Factory/Contents',
+        'plugin.BaserCore.Factory/ContentFolders',
+        'plugin.BaserCore.Factory/Permissions',
+        'plugin.BaserCore.Factory/Pages',
     ];
 
     /**
