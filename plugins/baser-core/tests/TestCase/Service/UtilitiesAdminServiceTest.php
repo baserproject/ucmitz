d6b6c43cebc2c5ea2b4f26a52b3da6a27c2c71b4
Chuong Le <35913113+zebeel@users.noreply.github.com>
2022-09-27 13:54:47 +0900

    fix #939 UtilitiesAdminService::getViewVarsForInfo() のユニットテスト

---
+++ plugins/baser-core/tests/TestCase/Service/UtilitiesAdminServiceTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BaserCore\Test\TestCase\Service;
+
+use BaserCore\Service\UtilitiesAdminService;
+use BaserCore\TestSuite\BcTestCase;
+
+/**
+ * Class UtilitiesAdminServiceTest
+ * @property UtilitiesAdminService $UtilitiesAdminService
+ */
+class UtilitiesAdminServiceTest extends BcTestCase
+{
+
+    /**
+     * Set Up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->UtilitiesAdminService = new UtilitiesAdminService();
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * test getViewVarsForInfo
+     */
+    public function test_getViewVarsForInfo()
+    {
+        $info = $this->UtilitiesAdminService->getViewVarsForInfo();
+        $this->assertArrayHasKey('datasource', $info);
+        $this->assertArrayHasKey('cakeVersion', $info);
+        $this->assertArrayHasKey('baserVersion', $info);
+    }
+
+}
