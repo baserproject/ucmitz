1047de33367c2c5856bc7be375d8b46069b010eb
Đỗ Văn Hùng <HungDV2022>
2022-11-02 18:28:36 +0900

    fix #1346 Api/ContentLinksController::delete() の APIとユニットテスト実装

---
+++ plugins/bc-content-link/tests/TestCase/Service/ContentLinksServiceTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BcSearchIndex\Test\TestCase\Service;
+
+use BcContentLink\Service\ContentLinksService;
+use BaserCore\TestSuite\BcTestCase;
+use BcContentLink\Service\ContentLinksServiceInterface;
+use BcContentLink\Test\Factory\ContentLinkFactory;
+use BaserCore\Utility\BcContainerTrait;
+
+/**
+ * Class ContentLinksServiceTest
+ * @property ContentLinksService $ContentLinksService
+ */
+class ContentLinksServiceTest extends BcTestCase
+{
+
+    use BcContainerTrait;
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'plugin.BcContentLink.Factory/ContentLinks',
+    ];
+
+    /**
+     * Set Up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->setFixtureTruncate();
+        parent::setUp();
+        $this->ContentLinksService = $this->getService(ContentLinksServiceInterface::class);
+    }
+
+    /**
+     * Tear Down
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        unset($this->ContentLinksService);
+        parent::tearDown();
+    }
+
+    /**
+     * test delete
+     */
+    public function test_delete()
+    {
+        ContentLinkFactory::make(['id' => 1, 'url' => '/test-delete'])->persist();
+        $this->assertTrue($this->ContentLinksService->delete(1));
+    }
+}
