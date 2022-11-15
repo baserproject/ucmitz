81d3dc3f3e3cbb9640a0f1a8f1380c40ca39a840
Chuong Le <35913113+zebeel@users.noreply.github.com>
2022-11-15 21:38:17 +0900

    fix #1457 fix error

--- plugins/bc-blog/tests/TestCase/Service/BlogCommentsServiceTest.php
+++ plugins/bc-blog/tests/TestCase/Service/BlogCommentsServiceTest.php
@@ -48,7 +48,6 @@ class BlogCommentsServiceTest extends BcTestCase
         $this->setFixtureTruncate();
         parent::setUp();
         $this->BlogCommentsService = new BlogCommentsService();
-        $this->loadFixtureScenario(BlogCommentsServiceScenario::class);
     }
 
     /**
@@ -82,6 +81,8 @@ class BlogCommentsServiceTest extends BcTestCase
      */
     public function testGet()
     {
+        $this->loadFixtureScenario(BlogCommentsServiceScenario::class);
+
         $comment = $this->BlogCommentsService->get(1);
         $this->assertEquals(1, $comment['id']);
         $this->assertEquals(1, $comment['blog_post']['id']);
