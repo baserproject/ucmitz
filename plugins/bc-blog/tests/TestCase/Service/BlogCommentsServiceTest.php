4aefc13e94f40d1aade4b7638068ddf5177266c2
Chuong Le <35913113+zebeel@users.noreply.github.com>
2022-11-21 16:16:58 +0900

    fix #1455 add import scenario

--- plugins/bc-blog/tests/TestCase/Service/BlogCommentsServiceTest.php
+++ plugins/bc-blog/tests/TestCase/Service/BlogCommentsServiceTest.php
@@ -13,6 +13,7 @@ namespace BcBlog\Test\TestCase\Service;
 
 use BaserCore\TestSuite\BcTestCase;
 use BcBlog\Service\BlogCommentsService;
+use BcBlog\Test\Scenario\BlogCommentsServiceScenario;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
