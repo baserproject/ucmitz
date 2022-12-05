9e96c25c213c40aa0b7d269c4435fbf0173e0cb7
Chuong Le <35913113+zebeel@users.noreply.github.com>
2022-12-05 14:46:55 +0900

    fix #1570 受信メール管理：一覧取得APIを実装

--- plugins/bc-mail/tests/TestCase/Controller/Admin/MailMessagesControllerTest.php
+++ plugins/bc-mail/tests/TestCase/Controller/Admin/MailMessagesControllerTest.php
@@ -11,10 +11,34 @@
 
 namespace BcMail\Test\TestCase\Controller\Admin;
 
+use BaserCore\Test\Scenario\InitAppScenario;
 use BaserCore\TestSuite\BcTestCase;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestTrait;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 class MailMessagesControllerTest extends BcTestCase
 {
+
+    /**
+     * ScenarioAwareTrait
+     */
+    use ScenarioAwareTrait;
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
+    ];
 
     /**
      * set up
@@ -23,7 +47,12 @@ class MailMessagesControllerTest extends BcTestCase
      */
     public function setUp(): void
     {
+        $this->setFixtureTruncate();
         parent::setUp();
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $token = $this->apiLoginAdmin(1);
+        $this->accessToken = $token['access_token'];
+        $this->refreshToken = $token['refresh_token'];
     }
 
     /**
@@ -57,7 +86,20 @@ class MailMessagesControllerTest extends BcTestCase
      */
     public function testIndex()
     {
-        $this->markTestIncomplete('このテストは、まだ実装されていません。');
+        // メールメッセージのデータを作成する
+        $mailMessageTable = TableRegistry::getTableLocator()->get('BcMail.MailMessages');
+        $mailContentId = 1;
+        $mailMessageTable->setup($mailContentId);
+        // mail_message_1テーブルに１件のレコードを追加する
+        $mailMessageTable->save(new Entity(['id' => 2]));
+
+        // 受信メール一覧のAPIを叩く
+        $this->get("/baser/api/bc-mail/mail_messages/index/$mailContentId.json?token=" . $this->accessToken);
+        // レスポンスのコードを確認する
+        $this->assertResponseOk();
+        // レスポンスのメールメッセージデータを確認する
+        $result = json_decode((string)$this->_response->getBody());
+        $this->assertNotEmpty($result->mailMessages);
     }
 
     /**
