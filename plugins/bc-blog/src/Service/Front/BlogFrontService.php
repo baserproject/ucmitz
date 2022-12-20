6d37cef5d0fb4bdfd1b655e69e2878a728058300
Đỗ Văn Hùng <HungDV2022>
2022-12-20 07:48:59 +0700

    fix #1479 BlogFrontService::getViewVarsForArchivesByAuthor のユニットテスト実装

--- plugins/bc-blog/src/Service/Front/BlogFrontService.php
+++ plugins/bc-blog/src/Service/Front/BlogFrontService.php
@@ -205,6 +205,7 @@ class BlogFrontService implements BlogFrontServiceInterface
      * @return array
      * @checked
      * @noTodo
+     * @unitTest
      */
     public function getViewVarsForArchivesByAuthor(ResultSet $posts, string $author, BlogContent $blogContent): array
     {
