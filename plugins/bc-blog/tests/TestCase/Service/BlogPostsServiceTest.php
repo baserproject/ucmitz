128fb280213fb999ff9382e4e138ae689f475b5a
Đỗ Văn Hùng <HungDV2022>
2022-12-23 08:57:18 +0700

    fix #1603　ユニットテスト実装を調整

--- plugins/bc-blog/tests/TestCase/Service/BlogPostsServiceTest.php
+++ plugins/bc-blog/tests/TestCase/Service/BlogPostsServiceTest.php
@@ -34,6 +34,11 @@ class BlogPostsServiceTest extends BcTestCase
      * @var array
      */
     public $fixtures = [
+        'plugin.BaserCore.Factory/Sites',
+        'plugin.BaserCore.Factory/SiteConfigs',
+        'plugin.BaserCore.Factory/Users',
+        'plugin.BaserCore.Factory/UsersUserGroups',
+        'plugin.BaserCore.Factory/UserGroups',
         'plugin.BcBlog.Factory/BlogPosts',
     ];
 
