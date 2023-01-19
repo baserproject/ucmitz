62b5e7fb528b8ad1a3279fdd40f55858373fb107
Đỗ Văn Hùng <HungDV2022>
2023-01-19 09:34:51 +0700

    fix #1772 テーマファイルAPI ファイル削除

--- plugins/bc-theme-file/tests/TestCase/Controller/Api/ThemeFilesControllerTest.php
+++ plugins/bc-theme-file/tests/TestCase/Controller/Api/ThemeFilesControllerTest.php
@@ -107,14 +107,6 @@ class ThemeFilesControllerTest extends BcTestCase
         //戻る値を確認
         $result = json_decode((string)$this->_response->getBody());
         $this->assertEquals('ファイル「base_name_1.php」の削除に失敗しました。', $result->message);
-
-        //POSTデータが空の場合、エラーを出る
-        $this->post('/baser/api/bc-theme-file/theme_files/delete.json?token=' . $this->accessToken, []);
-        //レスポンスコードを確認
-        $this->assertResponseCode(400);
-        //戻る値を確認
-        $result = json_decode((string)$this->_response->getBody());
-        $this->assertEquals('処理中にエラーが発生しました。', $result->message);
     }
 
     /**
