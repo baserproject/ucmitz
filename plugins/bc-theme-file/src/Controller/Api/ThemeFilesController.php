3fb3fe3bf6dde6afd7d5d7df017e63d01a10819e
Đỗ Văn Hùng <HungDV2022>
2023-02-06 15:19:19 +0900

    テーマファイルAPIを調整

--- plugins/bc-theme-file/src/Controller/Api/ThemeFilesController.php
+++ plugins/bc-theme-file/src/Controller/Api/ThemeFilesController.php
@@ -202,7 +202,7 @@ class ThemeFilesController extends BcApiController
 
         try {
             $data = $this->getRequest()->getQueryParams();
-            $data['fullpath'] = $this->getFullpath($data['theme'], $data['type'], $data['path']);
+            $data['fullpath'] = $service->getFullpath($data['theme'], $data['type'], $data['path']);
             $img = $service->getImg($data);
         } catch (BcFormFailedException $e) {
             $this->setResponse($this->response->withStatus(400));
