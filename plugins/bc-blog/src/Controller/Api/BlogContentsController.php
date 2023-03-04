bde95dd028a6d37f995a92b40bd9fda028802bbb
ryuring <egashira@catchup.co.jp>
2023-03-04 16:57:47 +0900

    Update BlogContentsController.php

--- plugins/bc-blog/src/Controller/Api/BlogContentsController.php
+++ plugins/bc-blog/src/Controller/Api/BlogContentsController.php
@@ -174,8 +174,8 @@ class BlogContentsController extends BcApiController
                 $message = __d('baser', '入力エラーです。内容を修正してください。');
             }
         } catch (RecordNotFoundException $e) {
-            $this->setResponse($this->response->withStatus(404));
-            $message = __d('baser', 'データが見つかりません。');
+            $this->setResponse($this->response->withStatus(500));
+            $message = __d('baser', 'データベース処理中にエラーが発生しました。');
         } catch (\Throwable $e) {
             $this->setResponse($this->response->withStatus(500));
             $message = __d('baser', 'データベース処理中にエラーが発生しました。' . $e->getMessage());
