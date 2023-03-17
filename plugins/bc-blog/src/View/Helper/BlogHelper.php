a08020b317ce389d67d82e43fce26a0cfb52b1a9
uchino <44802818+kk-uchino@users.noreply.github.com>
2023-03-17 15:52:19 +0900

    BlogHelperのgetTagを修正

--- plugins/bc-blog/src/View/Helper/BlogHelper.php
+++ plugins/bc-blog/src/View/Helper/BlogHelper.php
@@ -600,8 +600,8 @@ class BlogHelper extends Helper
         if ($options['tag'] === false) {
             $options['link'] = false;
         }
-        if (!empty($post['BlogTag'])) {
-            foreach($post['BlogTag'] as $tag) {
+        if (!empty($post->blog_tags)) {
+            foreach($post->blog_tags as $tag) {
                 if ($options['link']) {
                     $tags[] = $this->BcBaser->getLink($tag['name'], $this->getTagLinkUrl($crossingId, $tag, false), ['escape' => true]);
                 } else {
