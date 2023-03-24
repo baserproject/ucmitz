e4a4d85d9d86d994902492b65a1a30caf53d5cdb
Nghiem <nguyennghiem1205@gmail.com>
2023-03-24 16:06:31 +0700

    fix bug 2150

--- plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
@@ -17,6 +17,7 @@ use BaserCore\Test\Scenario\PermissionGroupsScenario;
 use BaserCore\TestSuite\BcTestCase;
 use BaserCore\Utility\BcContainerTrait;
 use BaserCore\Test\Factory\PermissionGroupFactory;
+use Cake\Core\Configure;
 use Cake\Datasource\Exception\RecordNotFoundException;
 use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
@@ -72,12 +73,25 @@ class PermissionGroupsServiceTest extends BcTestCase
     public function testBuild()
     {
         $this->loadFixtureScenario(PermissionGroupsScenario::class);
-        $result = $this->PermissionGroups->build(0, 'BaserCore');
+        $plugin = 'BaserCore';
+        $result = $this->PermissionGroups->build(0, $plugin);
         $this->assertCount(0, $result);
-        $result = $this->PermissionGroups->build(1, 'BaserCore');
-        $this->assertCount(24, $result);
-        $result = $this->PermissionGroups->build(1, 'BcBlog');
-        $this->assertCount(10, $result);
+
+        $this->PermissionGroups->build(1, $plugin);
+        $data = $this->PermissionGroups->getIndex(1, [])->where(['plugin' => $plugin])->all()->toArray();
+        Configure::load($plugin . '.permission', 'baser');
+        $settings = Configure::read('permission');
+        Configure::delete('permission');
+        $this->assertCount(count($settings), $data);
+
+        $plugin = 'BcBlog';
+        $this->PermissionGroups->build(1, $plugin);
+        $data = $this->PermissionGroups->getIndex(1, [])->where(['plugin' => $plugin])->all()->toArray();
+        Configure::load($plugin . '.permission', 'baser');
+        $settings = Configure::read('permission');
+        Configure::delete('permission');
+        $this->assertCount(count($settings), $data);
+
         $result = $this->PermissionGroups->build(1, 'Nghiem');
         $this->assertFalse($result);
     }
