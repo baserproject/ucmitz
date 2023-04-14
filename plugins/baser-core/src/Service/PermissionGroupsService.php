32003334f7720ad0a3b68e459c422af0c1f55378
Nghiem <nguyennghiem1205@gmail.com>
2023-04-14 13:19:54 +0700

    fix comment

--- plugins/baser-core/src/Service/PermissionGroupsService.php
+++ plugins/baser-core/src/Service/PermissionGroupsService.php
@@ -305,7 +305,7 @@ class PermissionGroupsService implements PermissionGroupsServiceInterface
 		$settings = Configure::read('permission');
 		if (!$settings) return false;
 
-        $result = [];
+        $result = true;
 		foreach($settings as $ruleGroupName => $setting) {
 
 			// PermissionGroup 存在確認、なければ作成
@@ -329,17 +329,18 @@ class PermissionGroupsService implements PermissionGroupsServiceInterface
 
 			// Permission 作成
 			foreach($setting['items'] as $ruleName => $item) {
-				$permissionsService->create([
-					'name' => isset($item['title'])? $item['title'] : $ruleName,
-					'permission_group_id' => $permissionGroup->id,
-					'user_group_id' => $userGroupId,
-					'url' => $item['url'],
-					'auth' => $item['auth'],
-					'method' => $item['method'],
-					'status' => true,
-				]);
-			}
-            $result[] = $permissionGroup;
+                if (!$permissionsService->create([
+                    'name' => isset($item['title']) ? $item['title'] : $ruleName,
+                    'permission_group_id' => $permissionGroup->id,
+                    'user_group_id' => $userGroupId,
+                    'url' => $item['url'],
+                    'auth' => $item['auth'],
+                    'method' => $item['method'],
+                    'status' => true,
+                ])) {
+                    $result = false;
+                }
+            }
 		}
 
 		Configure::delete('permission');
--- plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
+++ plugins/baser-core/tests/TestCase/Service/PermissionGroupsServiceTest.php
@@ -98,8 +98,12 @@ class PermissionGroupsServiceTest extends BcTestCase
     {
         $this->loadFixtureScenario(PermissionGroupsScenario::class);
         $plugin = 'BaserCore';
-        $result = $this->PermissionGroups->build(0, $plugin);
-        $this->assertCount(0, $result);
+        $this->PermissionGroups->build(0, $plugin);
+        $data = $this->PermissionGroups->getIndex(0, ['permission_amount' => true])
+            ->where(['plugin' => $plugin])
+            ->where(['permissions.user_group_id' => 0])
+            ->all()->toArray();
+        $this->assertCount(0, $data);
 
         $this->PermissionGroups->build(1, $plugin);
         $data = $this->PermissionGroups->getIndex(1, [])->where(['plugin' => $plugin])->all()->toArray();
